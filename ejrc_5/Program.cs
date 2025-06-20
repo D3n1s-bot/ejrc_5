ea1a73810aaaeeb38a0dcc131b5acbd431530cbd
Denis <74616294@idexperujapon.edu.pe>
2025-06-20 18:25:43 -0500

    Agregar archivos de proyecto.

---
+++ ejrc_5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejrc_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1,suma;
+            Console.WriteLine("Ingrese un numero: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            suma = num1;
+            while (num1!=0)
+            {
+                Console.WriteLine("Ingrese otro numero: ");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                suma=suma + num1;
+            }
+            Console.WriteLine("La suma de los numeros es:{0} " + suma);
+        }
+    }
+}
